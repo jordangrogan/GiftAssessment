07fe44e2bc5fd5d58d71750c4a6b2d2bc0c867ed
Jordan Grogan <jordangrogan11@gmail.com>
2019-01-17 21:35:32 -0500

    Questions clarification

--- qa.js
+++ qa.js
@@ -1,3 +1,5 @@
+// The questions should rotate in order of the gifts. For example, if there are 9 questions and 3 gifts, questions 0, 3, & 6 should correspond to gift 0.
+
 const questions = [
   {
     question: "I like organizing services and events."
